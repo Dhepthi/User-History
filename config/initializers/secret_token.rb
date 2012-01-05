89587fe0fcd4c00259ad10e85555e9e91193762f
Dhepthi <dhepthiln@spritle.com>
2012-01-05 15:26:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserHistory::Application.config.secret_token = '3bea7a993354d05c3ee77ad367ed2e279b873fb4a5f5d930c6e4efe927e55774a5fc1edea330556b0c14e030dd9e7dd6843c23fdd59e66bea1fe754af8679328'
